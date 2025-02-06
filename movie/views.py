c3ee480700fdd3506b8a001590b8dbf98a271a47
Maria-Olaya <mmolayal@eafit.edu.co>
2025-02-06 12:46:42 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home (request):
      #return HttpResponse('<h1>Welcome to Home Page</h1>')
      #return render(request, 'home.html')
-     return render(request, 'home.html', {'name':'Maria Olaya'})
+     #return render(request, 'home.html', {'name':'Maria Olaya'})
+     searchTerm = request.GET.get('searchMovie')
+     movies = Movie.objects.all()
+     return render (request, 'home.html', { 'searchTerm': searchTerm, 'movies': movies})
  
 def about (request):
     return render(request, 'about.html')
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -24,3 +27,5 @@ urlpatterns = [
     path('', movieViews.home),
     path('about/', movieViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
