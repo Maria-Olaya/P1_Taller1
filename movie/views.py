cdccb0969410ff71b012cdf9771e4516d2f73031
Maria-Olaya <mmolayal@eafit.edu.co>
2025-02-07 08:23:19 -0500

    correccion de busqueda de pelicula

--- movie/views.py
+++ movie/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Movie
+from .models import Movie 
 
 # Create your views here.
-def home (request):
+#def home (request):
      #return HttpResponse('<h1>Welcome to Home Page</h1>')
      #return render(request, 'home.html')
      #return render(request, 'home.html', {'name':'Maria Olaya'})
-     searchTerm = request.GET.get('searchMovie')
-     movies = Movie.objects.all()
-     return render (request, 'home.html', { 'searchTerm': searchTerm, 'movies': movies})
+     #searchTerm = request.GET.get('searchMovie')
+     #movies = Movie.objects.all()
+     #return render (request, 'home.html', { 'searchTerm': searchTerm, 'movies': movies})
+     
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
  
 def about (request):
     return render(request, 'about.html')
